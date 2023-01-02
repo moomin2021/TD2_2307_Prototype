4d6a4722e7358a315a1e689300c5997f46f07cce
チャン スギ <junjunchan64@gmail.com>
2023-01-03 06:29:44 +0900

    ボス攻撃3つ

--- Boss.cpp
+++ Boss.cpp
@@ -1,4 +1,6 @@
 #include "Boss.h"
+#include "stdio.h"
+#include "time.h"
 
 void Boss::Initialize()
 {
@@ -9,21 +11,106 @@ void Boss::Initialize()
 
 void Boss::Update()
 {
-	if (coolTime_ >= 30) {
-		if (pos_.x - size_ < 0) {
-			speed_ = Vector2(2, 0);
+	srand(time(NULL));
+	int pattern = rand() % 30;
+	//int pattern = 4;
+
+	if (--coolTime_ <= 0) {
+		if (pattern == 0) {
+			dash_ = { 20,0 };
+			coolTime_ = SetTime(1);
 		}
-		if (pos_.x + size_ > 1000) {
-			speed_ = Vector2(-2, 0);
+		else if (pattern == 1) {
+			dash_ = { 0,20 };
+			coolTime_ = SetTime(1);
 		}
-		pos_ += speed_;
+		else if (pattern == 2) {
+			dash_ = { -20,0 };
+			coolTime_ = SetTime(1);
+		}
+		else if (pattern == 3) {
+			dash_ = { 0,-20 };
+			coolTime_ = SetTime(1);
+		}
+		else if (pattern >= 4 && pattern <= 10) {
+			isJump_ = true;
+		}
+		else {
+
+			BulletShot(Vector2(0, 3));
+			BulletShot(Vector2(3, 0));
+			BulletShot(Vector2(0, -3));
+			BulletShot(Vector2(-3, 0));
+			BulletShot(Vector2(2.4f, 2.4f));
+			BulletShot(Vector2(-2.4f, -2.4f));
+			BulletShot(Vector2(2.4f, -2.4f));
+			BulletShot(Vector2(-2.4f, 2.4f));
+			coolTime_ = SetTime(2);
+		}
 
 	}
-	if (--coolTime_ <= 0) {
-		std::unique_ptr<EnemyBullet> newBullet = std::make_unique<EnemyBullet>();
-		newBullet->Initialize(pos_, Vector2(3, 0));
-		bullet_.push_back(std::move(newBullet));
-		coolTime_ = setCoolTime_;
+
+	pos_ += dash_;
+	if (dash_.x < 0) {
+		dash_.x++;
+		if (pos_.x < 0 + size_) {
+			pos_.x = 0 + size_;
+		}
+	}
+	else if (dash_.x > 0) {
+		dash_.x--;
+		if (pos_.x > 1000 - size_) {
+			pos_.x = 1000 - size_;
+		}
+	}
+	if (dash_.y < 0) {
+		dash_.y++;
+		if (pos_.y < 0 + size_) {
+			pos_.y = 0 + size_;
+		}
+	}
+	else if (dash_.y > 0) {
+		dash_.y--;
+		if (pos_.y > 1000 - size_) {
+			pos_.y = 1000 - size_;
+		}
+	}
+
+	if (isJump_ == true) {
+		if (size_ == 10) {
+			jumpTime_--;
+			if (jumpTime_ == 0) {
+				isJump_ = false;
+				jumpTime_ = SetTime(3);
+				coolTime_ = SetTime(5);
+			}
+		}
+		else {
+			size_--;
+		}
+		Vector2 len = playerPos_ - pos_;
+		len.normalize();
+		pos_ += len * 3;
+	}
+	else {
+		if (size_ < 50) {
+			size_++;
+		}
+		if (size_ == 49) {
+			isImpact_ = true;
+		}
+	}
+
+	if (isImpact_ == true) {
+		impactSize_ += 10;
+		if (impactSize_ >= 100) {
+			isImpact_ = false;
+		}
+	}
+	else {
+		if (impactSize_ > 0) {
+			impactSize_ -= 10;
+		}
 	}
 
 	for (std::unique_ptr<EnemyBullet>& bullet : bullet_) {
@@ -34,8 +121,27 @@ void Boss::Update()
 void Boss::Draw()
 {
 	//ƒ{ƒX–{‘Ì‚Ì•`‰æ
-	DrawBox(pos_.x - size_, pos_.y - size_, pos_.x + size_, pos_.y + size_, GetColor(200, 0, 0),true);
+	if (size_ == 50.0f) {
+		DrawBox(pos_.x - size_, pos_.y - size_, pos_.x + size_, pos_.y + size_, GetColor(200, 0, 0), true);
+	}
+	else {
+		DrawCircle(pos_.x, pos_.y, size_, GetColor(50, 50, 50), true);
+	}
+	DrawCircle(pos_.x, pos_.y, impactSize_, GetColor(250, 250, 0), true);
+
 	for (std::unique_ptr<EnemyBullet>& bullet : bullet_) {
 		bullet->Draw();
 	}
+}
+
+void Boss::BulletShot(Vector2 way)
+{
+	std::unique_ptr<EnemyBullet> newBullet = std::make_unique<EnemyBullet>();
+	newBullet->Initialize(pos_, Vector2(way));
+	bullet_.push_back(std::move(newBullet));
+}
+
+int Boss::SetTime(int sec)
+{
+	return sec * 60;
 }
--- Boss.h
+++ Boss.h
@@ -15,6 +15,14 @@ public:
 	//描画
 	void Draw();
 
+	void BulletShot(Vector2 way);
+
+	int SetTime(int sec);
+
+	void SetPlayerPos(Vector2 pos) {
+		playerPos_ = pos;
+	}
+
 private:
 	//座標
 	Vector2 pos_;
@@ -22,10 +30,24 @@ private:
 	float size_ = 50.0f;
 
 	//弾の間隔
-	uint32_t setCoolTime_ = 5 * 60;
+	uint32_t setCoolTime_ = 2 * 60;
 	uint32_t coolTime_ = setCoolTime_;
 	Vector2 speed_;
-
+	//弾
 	std::list<std::unique_ptr<EnemyBullet>> bullet_;
+
+	//突進
+	Vector2 dash_ = { 0,0 };
+
+	//ジャンプ攻撃
+	bool isJump_;
+	int jumpTime_ = 3 * 60;
+	int impactSize_;
+	bool isImpact_;
+
+	//プレイヤーの位置
+	Vector2 playerPos_;
+	
+
 };
 
